73a4c3ed6a6b26a6a6b3e8ffe1e06b503c476197
shreks99 <shreyk1999@gmail.com>
2023-11-07 16:32:19 -0800

    Initial Setup

---
+++ src/main/java/sks/project/grader/GraderApplication.java
@@ -0,0 +1,13 @@
+package sks.project.grader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sks/project/grader/GraderApplicationTests.java
@@ -0,0 +1,13 @@
+package sks.project.grader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
